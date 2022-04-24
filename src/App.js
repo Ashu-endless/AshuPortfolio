b9cea37607b33fbac08292b96e73692e7613028a
Ashu Endless <ashuendless28@gmail.com>
2022-04-24 21:25:17 +0530

    initial

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import ScrollAnimation from 'react-animate-on-scroll';
+import SectionWipes from './SectionWipes';
+import SectionWipes2 from './SectionWipes2';
+function App() {
+  return (
+    <SectionWipes2/>
+    // <SectionWipes/>
+  );
+}
+
+export default App;
---
+++ src/sections/Myworks.jsx
@@ -0,0 +1,11 @@
+import './MyWorks.css';
+
+
+
+function MyWorks() {
+  return (
+   <div className='section' id='section-myworks' ></div>
+  );
+}
+
+export default MyWorks;
